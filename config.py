92904575a3a6a84a3537ae9c026263097e00bfcf
ksu <korchagina_kseniaaa@mail.ru>
2025-10-05 22:04:33 +0300

    [all-6] fix connection with ml, llm

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # PREDICT_COLOR_TYPE_URL = "http://host.docker.internal:8000/predict_color_type"
 # PREDICT_COLOR_TYPE_LLM_URL = "http://host.docker.internal:8001/recommend_by_color_type"
 
-PREDICT_BODY_TYPE_ML_URL = "http://ml:8000/predict"
+PREDICT_BODY_TYPE_ML_URL = "http://ml:8000/predict_body_type"
 PREDICT_BODY_TYPE_LLM_URL = "http://llm:8001/recommend"
 
 PREDICT_COLOR_TYPE_URL = "http://ml:8000/predict_color_type"
